d2170176b4c461c30b9b9746d7e620cf8cc78326
mudblood97 <65904932+mudblood97@users.noreply.github.com>
2021-11-30 23:08:41 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginForm from "../pages/LoginForm";
+import Home from "../pages/Home";
+import DetailAddKaryawan from "../pages/DetailAddKaryawan";
+import DetailEditKaryawan from "../pages/DetailEditKaryawan";
 
-const stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const Router = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="LoginForm" component={LoginForm} options={{ headerShown: false }}/>
+            <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+            <Stack.Screen name="DetailAddKaryawan" component={DetailAddKaryawan} options={{ headerShown: false }}/>
+            <Stack.Screen name="DetailEditKaryawan" component={DetailEditKaryawan} options={{ headerShown: false }}/>
         </Stack.Navigator>
     )
 }
