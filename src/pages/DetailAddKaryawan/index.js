68bb19447a558e1228e73a7534c8ff4c98c03106
mudblood97 <65904932+mudblood97@users.noreply.github.com>
2021-11-30 23:18:01 +0700

    Update index.js

--- src/pages/DetailAddKaryawan/index.js
+++ src/pages/DetailAddKaryawan/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { Text, StyleSheet, View, TextInput, TouchableOpacity, Alert } from 'react-native'
 import RNPickerSelect from 'react-native-picker-select';
 import DatePicker from 'react-native-datepicker';
+import axios from 'axios';
 
 export default class DetailAddKaryawan extends Component {
     constructor(props) {
@@ -28,6 +29,44 @@ export default class DetailAddKaryawan extends Component {
         console.log(this.state.gender);
         console.log(this.state.dateOfBirth);
         
+        axios
+        .post('http://gmedia.bz/DemoCase/main/add_karyawan', {
+            nama: this.state.name,
+            alamat: this.state.address,
+            gender: this.state.gender,
+            tgl_lahir: this.state.dateOfBirth,
+        }, {
+            headers: {
+                'Client-Service': `gmedia-recruitment`,
+                'Auth-Key': `demo-admin`,
+                'User-Id': `1`,
+                'Token': `8godoajVqNNOFz21npycK6iofUgFXl1kluEJt/WYFts9C8IZqUOf7rOXCe0m4f9B`
+            }
+        })
+            .then(response => {
+            console.log('response data : ', response.data.metadata);
+            console.log('response status : ', response.data.metadata.status);
+
+            if (response.data.metadata.status === 200) {
+                // console.log('BERHASIL >>>', usernameInput, passwordInput, response);
+                // console.log('Masuk if status 200');
+                // this.props.navigation.navigate('DetailLaryawan');
+                Alert.alert('OK ', response.data.metadata.message);
+                this.props.navigation.navigate('Home');
+
+            } else {
+                console.log(
+                    'Peringatan >>>',
+                    response.data.metadata,
+                );
+                // Alert.alert('Username/Password Salah ', response.data.error.msg);
+                Alert.alert(response.data.metadata.message);
+            }
+        })
+        .catch(error => {
+            console.log('ERROR CATCH >>>', response.data.metadata.message);
+            Alert.alert('Username/Password Salah ');
+        });
     }
 
     render() {
